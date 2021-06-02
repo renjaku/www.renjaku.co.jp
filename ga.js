eac22ea2ab65d30f5c9bf85cf5a52bf5b22d1bcb
Oshinko <osnk@renjaku.jp>
2021-06-02 19:27:36 +0900

    Add support for Google Analytics

---
+++ ga.js
@@ -0,0 +1,8 @@
+const TARGET = 'G-NHWWDSSJ9Y'
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', TARGET);
+const script = document.createElement('script')
+script.src = 'https://www.googletagmanager.com/gtag/js?id=' + TARGET
+document.querySelector('head').append(script)
