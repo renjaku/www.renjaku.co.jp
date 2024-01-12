321de870ed76b6dcb2edeb0da1cac05dcd126bbc
Shintaro Maeda <maesin@renjaku.co.jp>
2024-01-12 11:52:36 +0900

    給与明細作成サービスを追加

--- ga.js
+++ ga.js
@@ -1,8 +1,10 @@
-const TARGET = 'G-NHWWDSSJ9Y'
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', TARGET);
-const script = document.createElement('script')
-script.src = 'https://www.googletagmanager.com/gtag/js?id=' + TARGET
-document.querySelector('head').append(script)
+if (window.location.hostname !== 'localhost') {
+  const TARGET = 'G-NHWWDSSJ9Y';
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', TARGET);
+  const script = document.createElement('script');
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=' + TARGET;
+  document.querySelector('head').append(script);
+}
