9cb0ec6d022c07bf3f9f7aef31ad8b47f1fab89b
Shintaro Maeda <maesin@renjaku.co.jp>
2025-12-01 15:24:01 +0900

    問い合わせフォームを更新

--- src/contact.js
+++ src/contact.js
@@ -2,9 +2,13 @@ function rot13(s) {
   return s.replace(/[a-zA-Z]/g, c => String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26));
 }
 
-function send() {
-  let email = document.querySelector('input[type=text]');
-  let message = document.querySelector('textarea');
+async function send(event) {
+  event.preventDefault();
+
+  const fieldset = event.target.querySelector('fieldset');
+  const email = event.target.querySelector('input[type=text]');
+  const message = event.target.querySelector('textarea');
+  const sales = event.target.querySelector('input[type=checkbox]');
 
   const validErrors = [];
   const emailValue = email.value.trim();
@@ -26,19 +30,38 @@ function send() {
     !window.confirm('弊社からの返信を受け取るメールアドレスが入力されていませんが、よろしいですか？')
   ) return;
 
-  let req = new XMLHttpRequest();
-  req.open('POST', rot13('uggcf://qvfpbeq.pbz/ncv/jroubbxf/932997533146415195/jtRIrb_d030zHkuXXSC6p-aEp4eOyKDkBdVn9eJBMGnlh8ZSs7xy7-8XCpAcHnGKmWrG'));
-  req.setRequestHeader('Content-Type', 'application/json');
-  req.onload = () => window.alert('送信が完了しました。内容を確認次第ご返信いたしますので、しばらくお待ちください。');
-  req.onerror = () =>
-    window.alert('There was an error processing your request. ' +
-                 'Please wait and try again later.');
+  const endpoint = rot13(
+    sales.checked ?
+      'uggcf://qvfpbeq.pbz/ncv/jroubbxf/1444927091404701718/wKObdjrsl1D3tsz7Whzp6_QkDBqkwPQ42ILUjcKPdVVScEVdyehm74gTX821eQYViYoi' :
+      'uggcf://qvfpbeq.pbz/ncv/jroubbxf/1444884058935922789/LIPs7aKFssswp-vsYQptXRAWGtDNz0y0cy2JmBZsV29dj8UdIVP2CIQVm2YW2hs8Zu5B'
+  );
+
   const content = `
 @everyone ${messageValue}
 
 Reply-To: ${emailValue}
 `.trim();
-  req.send(JSON.stringify({ content }));
+
+  fieldset.disabled = true;
+
+  try {
+    const res = await fetch(endpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ content })
+    });
+
+    if (!res.ok) {
+      window.alert(`HTTP Error: ${res.status}: ${res.statusText}`);
+      return;
+    }
+
+    window.alert('送信が完了しました。内容を確認次第ご返信いたしますので、しばらくお待ちください。');
+  } catch {
+    window.alert('予期せぬエラーです。時間をおいて再度お試しください。');
+  } finally {
+    fieldset.disabled = false;
+  }
 }
 
 window.Contact = { send };
