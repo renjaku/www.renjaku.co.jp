6c2375ce29eecb1d2499c44ab088628d0d82714d
Shintaro Maeda <maesin@renjaku.co.jp>
2026-06-16 10:55:01 +0900

    依存関係解決エラーを修正

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,11 @@ import { prerenderRoutes } from "./routes-manifest";
 export default {
   ssr: false,
   prerender: [...prerenderRoutes],
+  future: {
+    v8_middleware: true,
+    v8_splitRouteModules: true,
+    v8_viteEnvironmentApi: true,
+    v8_passThroughRequests: true,
+    v8_trailingSlashAwareDataRequests: true,
+  },
 } satisfies Config;
